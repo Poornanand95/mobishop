a67b23a5b6896cde3e525319708d78b3ca6348c7
Poornanand Verma <poornanand95@gmail.com>
2021-05-18 01:42:32 +0530

    third commit

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Fragment } from 'react';
+import '../components/css/SignUp.css';
 
 function SignUp() {
-  return <div>Sign Up here</div>;
+  return (
+    <Fragment>
+      <div className='signup-form-container'>
+        <form className='signup-form'>
+          <div className='signup-label-input'>
+            <label for='admin-fname'>Admin first name:</label>
+            <input type='text' id='admin-fname' />
+          </div>
+          <div className='signup-label-input'>
+            <label for='admin-lname'>Admin last name:</label>
+            <input type='text' id='admin-lname' />
+          </div>
+          <div className='signup-label-input'>
+            <label for='password'>Password:</label>
+            <input type='text' id='password' />
+          </div>
+          <div className='signup-signup-wrapper'>
+            <div className='signup-label-input'>
+              <input
+                className='sign-up-btn btn'
+                type='submit'
+                value='Sign Up'
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </Fragment>
+  );
 }
 
 export default SignUp;
